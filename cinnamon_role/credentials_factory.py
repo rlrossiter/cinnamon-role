d1c82eaf1e4b389160a196e6fdbf9dcd28cea3aa
Ryan Rossiter <rlrossit@us.ibm.com>
2016-02-24 03:20:09 +0000

    Use exact role matching credentials provider
    In the tempest preprovisioned credentials provider, when it looks for
    users to get by role, it gets any user with those roles assigned. These
    users may have extra roles added. Because we need to check against users
    that have an exact specified role, we must be more restrictive in our
    user selection.
    
    More of the Tempest infrastructure needed to be swapped out in order to
    return the new preprovisioned credential provider, and helper
    methods/decorators were added appropriately to add that functionality.

---
+++ cinnamon_role/credentials_factory.py
@@ -0,0 +1,74 @@
+from tempest.common import credentials_factory
+from tempest import config
+
+from cinnamon_role import preprov_creds as ppc
+
+CONF = config.CONF
+
+
+def with_role_matching_credentials(cls):
+    class _DecoratedClass(cls):
+        @classmethod
+        def _get_credentials_provider(cls):
+            if (not hasattr(cls, '_creds_provider') or not cls._creds_provider or
+                    not cls._creds_provider.name == cls.__name__):
+                force_tenant_isolation = getattr(cls, 'force_tenant_isolation',
+                                                 False)
+
+                cls._creds_provider = get_credentials_provider(
+                    name=cls.__name__, network_resources=cls.network_resources,
+                    force_tenant_isolation=force_tenant_isolation,
+                    identity_version=cls.get_identity_version())
+            return cls._creds_provider
+
+        @classmethod
+        def are_roles_available(cls, roles):
+            cred_provider = cls._get_credentials_provider()
+
+            # Only check if the roles are available if we're using
+            # the custom cred provider, otherwise return True
+            # so we don't hold things up
+            if hasattr(cred_provider, 'are_roles_available'):
+                return cred_provider.are_roles_available(roles)
+            else:
+                return True
+
+        @classmethod
+        def get_client_manager(cls, credential_type=None, roles=None,
+                               force_new=None):
+            if roles:
+                if not cls.are_roles_available(roles):
+                    skip_msg = (
+                            "%s skipped because the configured credential provider"
+                            " is not able to provide credentials with the roles "
+                            "%s assigned" % (cls.__name__, roles))
+                    raise cls.skipException(skip_msg)
+            return super(_DecoratedClass, cls).get_client_manager(
+                credential_type=credential_type, roles=roles, force_new=force_new)
+
+    # In order to wrap cleanly, set the name of the decorated class back
+    # to what it was before
+    _DecoratedClass.__name__ = cls.__name__
+    return _DecoratedClass
+
+
+def get_credentials_provider(name, network_resources=None,
+                             force_tenant_isolation=False,
+                             identity_version=None):
+    provider = credentials_factory.get_credentials_provider(
+        name, network_resources=network_resources,
+        force_tenant_isolation=force_tenant_isolation,
+        identity_version=identity_version)
+
+    if CONF.auth.test_accounts_file:
+        return ppc.ExactRoleMatchingPreProvisionedCredentialProvider(
+            name=name, identity_version=identity_version,
+            **credentials_factory._get_preprov_provider_params())
+    else:
+        return provider
+
+
+
+
+
+
---
+++ cinnamon_role/preprov_creds.py
@@ -0,0 +1,37 @@
+from tempest_lib import exceptions as lib_exc
+from tempest.common import preprov_creds
+
+
+class ExactRoleMatchingPreProvisionedCredentialProvider(
+    preprov_creds.PreProvisionedCredentialProvider):
+
+    def _get_match_hash_list(self, roles=None):
+        temp_hashes = super(
+            ExactRoleMatchingPreProvisionedCredentialProvider,
+            self)._get_match_hash_list(roles=roles)
+        # search through the roles we don't want because if we find a user
+        # in those roles, we don't want the user. We need to make an exact
+        # match
+        unwanted_roles = set(self.hash_dict['roles'].keys()) - set(roles)
+        for role in unwanted_roles:
+            unwanted_users = self.hash_dict['roles'].get(role)
+            users_to_remove = set(temp_hashes) & set(unwanted_users)
+            temp_hashes = set(temp_hashes) - set(users_to_remove)
+
+        if not temp_hashes:
+            raise lib_exc.InvalidCredentials(
+                "No credentials with exact roles %s specified in the "
+                "accounts file" % roles)
+
+        return temp_hashes
+
+    def are_roles_available(self, roles):
+        try:
+            hashes = self._get_match_hash_list(roles=roles)
+        except lib_exc.InvalidCredentials:
+            return False
+
+        if hashes:
+            return True
+        else:
+            return False
